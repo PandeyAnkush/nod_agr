b2b61d6115f3c5a19cd4da23b00c0c6da8900cd7
PandeyAnkush <ankushapndey92@gmial.com>
2021-10-28 12:32:22 +0530

    Sql data update using api updated

--- get_1.js
+++ get_1.js
@@ -1,9 +1,8 @@
 const exp = require ('express');
 const app = exp();
 const hw ='hello world!!';
-const con =require ('./sql_mod');
 const midd =require('./middleWare');
-
+app.use(exp.json());
 
 
 app.get('/',function(req,res)
@@ -11,5 +10,6 @@ app.get('/',function(req,res)
 	res.send(hw);
 });
 
-app.get('/sql',midd.validate)
+app.get('/sql',midd.get_api);
+app.put('/sql',midd.put_api);
 app.listen(3000);
--- middleWare.js
+++ middleWare.js
@@ -1,11 +1,19 @@
+// get_api to take the id as pramas and retrun data
+// put_api to take id from query param and check if it is exist if not than make new entry
+//data is taken from body
+//caller funtion is get_1.js
+
 const con =require ('./sql_mod');
 let my_middleware = {
-	validate: function(req,res,next){
+	get_api: function(req,res,next){
 		let id =  req.query.id;
 		if(isNaN(id)){
 			res.send('Id must be a number');
 		}
 		else{
+			console.log(id);
+			res.send(id);
+			
 			let q ='select * from city where id =' + id;
 			console.log(q);
 			con.query(q, function(err,result,fields){
@@ -21,6 +29,45 @@ let my_middleware = {
 			);
 		}
 		
+	},
+	put_api: function(req,res,next){
+		let id = req.query.id;
+		if(isNaN(id)){
+			res.send('Id must be a number');
+		}
+		else{
+
+			let id = req.body.identity;
+			let name = '"' +req.body.name+'"' ;
+			let countrycode = '"' +req.body.countrycode+'"' ;
+			let dist = '"' +req.body.dist+'"' ;
+			let population = req.body.pop;
+			//console.log(req.body.identity);
+			//console.log(name);
+			//console.log(countrycode);
+			//console.log(dist);
+			//console.log(pop);
+			//res.send(req.body);
+			let identity=req.query.id;
+			let q = "select * from city where id ="+ req.query.id;
+			con.query(q,function(err,result, field){
+				if(err) throw err;
+				if(!result.length){
+				q='insert into city (ID,Name,CountryCode,District,Population)values ('+id+','+name+','+countrycode+','+dist+','+population+');	';
+				q='insert into city (ID,Name,CountryCode,District,Population)values ('+id+','+name+','+countrycode+','+dist+','+population+');';
+				//console.log(q);
+				con.query(q,function(err,result,fields){
+				if(err)throw(err);
+				console.log('Data Updated');
+				res.send('Data updated'); 	
+				})
+				}
+				else {
+					res.send('User already exists');
+					return next();
+				}
+			});
+		}
 	}
 }
 module.exports = my_middleware;
